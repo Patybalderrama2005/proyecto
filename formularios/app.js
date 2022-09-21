87671c2208ac7a21f90714c305dbd10d612181ef
patybalderrama2005 <nellypatriciabalderramaalanis@gmail.com>
2022-09-21 08:49:20 -0500

    HOLA

---
+++ formularios/app.js
@@ -0,0 +1,77 @@
+const imprimir=()=>{
+    var folio = document.querySelector("#folio").value;
+    var fecha = document.querySelector("#fecha").value;
+    var aciertos = document.querySelector("#aciertos").value;
+    var carrera = document.querySelector("#carrera").value;
+    var nombre = document.querySelector("#nombre").value;
+    var ap = document.querySelector("#ap").value;
+    var am = document.querySelector("#am").value;
+    var dia = document.querySelector("#dia").value;
+    var mes = document.querySelector("#mes").value;
+    var año = document.querySelector("#año").value;
+    var lugarnacimiento = document.querySelector("#lugarnacimiento").value;
+    var curp = document.querySelector("#curp").value;
+    var sexo = document.querySelector("#sexo").value;
+    var alergia = document.querySelector("#alergia").value;
+    var telaspirante = document.querySelector("#telaspirante").value;
+    var teltutor = document.querySelector("#teltutor").value;
+    var emailaspirante = document.querySelector("#emailaspirante").value;
+    var emailtutor = document.querySelector("#emailtutor").value;
+    var calle = document.querySelector("#calle").value;
+    var numero = document.querySelector("#numero").value;
+    var colonia = document.querySelector("#colonia").value;
+    var localidad = document.querySelector("#localidad").value;
+    var cp = document.querySelector("#cp").value;
+    var municipio = document.querySelector("#municipio").value;
+    var estado = document.querySelector("#estado").value;
+    var nombreescuela = document.querySelector("#nombreescuela").value;
+    var cct = document.querySelector("#cct").value;
+    var promedio = document.querySelector("#promedio").value;
+    var añoingreso = document.querySelector("#añoingreso").value;
+    var añoegreso = document.querySelector("#añoegreso").value;
+    var nombret = document.querySelector("#nombret").value;
+    var apt = document.querySelector("#apt").value;
+    var amt = document.querySelector("#amt").value;
+    var curpt = document.querySelector("#curpt").value;
+    var ultimogrado = document.querySelector("#ultimogrado").value;
+
+
+
+
+    document.querySelector("#Sfolio").innerHTML= folio;
+    document.querySelector("#Sfecha").innerHTML= fecha;
+    document.querySelector("#Saciertos").innerHTML= aciertos;
+    document.querySelector("#Scarrera").innerHTML= carrera;
+    document.querySelector("#Snombre").innerHTML= nombre;
+    document.querySelector("#Sap").innerHTML= ap;
+    document.querySelector("#Sam").innerHTML= am;
+    document.querySelector("#Sdia").innerHTML= dia;
+    document.querySelector("#Smes").innerHTML= mes;
+    document.querySelector("#Saño").innerHTML= año;
+    document.querySelector("#Slugarnacimiento").innerHTML= lugarnacimiento;
+    document.querySelector("#Scurp").innerHTML= curp;
+    document.querySelector("#Ssexo").innerHTML= sexo;
+    document.querySelector("#Salergia").innerHTML= alergia;
+    document.querySelector("#Stelaspirante").innerHTML= telaspirante;
+    document.querySelector("#Steltutor").innerHTML= teltutor;
+    document.querySelector("#Semailaspirante").innerHTML= emailaspirante;
+    document.querySelector("#Semailtutor").innerHTML= emailtutor;
+    document.querySelector("#Scalle").innerHTML= calle;
+    document.querySelector("#Snumero").innerHTML= numero;
+    document.querySelector("#Scolonia").innerHTML= colonia;
+    document.querySelector("#Slocalidad").innerHTML= localidad;
+    document.querySelector("#Scp").innerHTML= cp;
+    document.querySelector("#Smunicipio").innerHTML= municipio;
+    document.querySelector("#Sestado").innerHTML= estado;
+    document.querySelector("#Snombreescuela").innerHTML= nombreescuela;
+    document.querySelector("#Scct").innerHTML= cct;
+    document.querySelector("#Spromedio").innerHTML= promedio;
+    document.querySelector("#Sañoingreso").innerHTML= añoingreso;
+    document.querySelector("#Sañoegreso").innerHTML= añoegreso;
+    document.querySelector("#Snombret").innerHTML= nombret;
+    document.querySelector("#Sapt").innerHTML= apt;
+    document.querySelector("#Samt").innerHTML= amt;
+    document.querySelector("#Scurpt").innerHTML= curpt;
+    document.querySelector("#Sultimogrado").innerHTML= ultimogrado;
+
+}
